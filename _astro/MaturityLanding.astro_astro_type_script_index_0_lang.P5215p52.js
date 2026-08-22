b824088271cb1cc10d14d0643e17b2e19b3032f1
Grow Business <growbusinessadm@gmail.com>
2026-08-22 14:09:30 -0300

    Publish maturity cache fix

---
+++ _astro/MaturityLanding.astro_astro_type_script_index_0_lang.P5215p52.js
@@ -0,0 +1,1 @@
+import{l as e,s as r,a}from"./maturity-state.D-TL1SGm.js";e()||r(a({href:window.location.href,referrer:document.referrer}));
---
+++ _astro/index.astro_astro_type_script_index_0_lang.Bct0Cv_5.js
@@ -0,0 +1,1 @@
+import{l as e,g as o,L as t,w as s}from"./maturity-state.D-TL1SGm.js";const a=e();(!a||o(a)===t)&&window.location.replace(s(t));
